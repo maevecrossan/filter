578b7862c0a24954126277f5322586aed69fd398
Maeve Crossan <maevecrossan@gmail.com>
2024-09-15 19:35:39 +0000

    Add challenge code

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);  // prints:  [ { name: 'Michael', age: 23 } ]
+
+const paul = people.filter(p => p.name === "Paul")[0]; //person shortened to p. [0] accesses object.
+console.log(paul);   // prints:  { name: 'Paul', age: 18 }
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,42 @@ const students = [
     ]
   },
 ];
+
+// filter challenge
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+
+const candidates = students.filter(hasStrongSkills);
+const names = candidates.map(candidates => candidates.name)
+console.log(names); // to get only names of candidates
+
+
+let students1 = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students1.filter(student => student.results.maths >= 5);
+console.log(topMaths);
